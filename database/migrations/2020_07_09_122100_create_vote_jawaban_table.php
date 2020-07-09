00f6ac5c6b3f82e06db346844811ade6fc12bc52
Rivan <43029672+Posan20@users.noreply.github.com>
2020-07-09 19:48:59 +0700

    Penambahan migration tabel, template adminlte, laravel auth

---
+++ database/migrations/2020_07_09_122100_create_vote_jawaban_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoteJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vote_jawaban', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('value')->nullable();
+            $table->unsignedBigInteger('jawaban_id');
+            $table->foreign('jawaban_id')->references('id')->on('jawaban')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vote_jawaban');
+    }
+}
